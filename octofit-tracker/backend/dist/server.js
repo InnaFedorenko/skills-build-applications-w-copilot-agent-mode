42027c580e9bbff31872deeee1718988e22ecd22
Inna Fedorenko <ivf.fedorenko@gmail.com>
2026-07-07 18:01:56 +0000

    Sync backend build output

--- octofit-tracker/backend/dist/server.js
+++ octofit-tracker/backend/dist/server.js
@@ -4,14 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createApp = void 0;
+exports.getApiBaseUrl = getApiBaseUrl;
 exports.startServer = startServer;
 const mongoose_1 = __importDefault(require("mongoose"));
 const app_1 = require("./app");
 var app_2 = require("./app");
 Object.defineProperty(exports, "createApp", { enumerable: true, get: function () { return app_2.createApp; } });
+function getApiBaseUrl() {
+    const codespaceName = process.env.CODESPACE_NAME;
+    return codespaceName
+        ? `https://${codespaceName}-8000.app.github.dev`
+        : 'http://localhost:8000';
+}
 const app = (0, app_1.createApp)();
 const port = Number(process.env.PORT || 8000);
 const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/octofit_db';
+const apiBaseUrl = getApiBaseUrl();
 mongoose_1.default
     .connect(mongoUri)
     .then(() => {
@@ -23,6 +31,7 @@ mongoose_1.default
 function startServer() {
     return app.listen(port, () => {
         console.log(`Backend listening on port ${port}`);
+        console.log(`API base URL: ${apiBaseUrl}`);
     });
 }
 if (require.main === module) {
